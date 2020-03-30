3adc9f7bfd1252aaab3682b09ba4d87f5d36271d
NamHoaiNguyen <nguyen.hoai.nam.3912@gmail.com>
2020-03-30 18:44:38 +0700

    week3

---
+++ week3/week3.1/ConditionalTest.php
@@ -0,0 +1,58 @@
+<html>
+    <head><title>Conditional Test</title></head>
+<body>
+    <?php
+        $first=$_GET["firstName"]; $middle = $_GET["middleName"]; $last = $_GET["lastName"];
+        print("Hi $first! Your full name is $last $middle $first. <br>");
+        if ($first == $last){
+            print("$first and $last are equal");
+        }
+        if ($first < $last){
+            print("$first is less than $last");
+        }
+        if ($first > $last){
+            print ("$first is greater than $last");
+        }
+        print("<br></br>");
+        
+        $grade1 = $_GET["grade1"]; $grade2 = $_GET["grade2"];
+        $final = (2*$grade1+3*$grade2)/5;
+        
+        if ($final > 89){
+            print ("Your final grade is $final. You got an A. Congratulations!");
+            $rate = "A";
+        }
+        else if ($final > 79){
+            print ("Your final grade is $final. You got an B.");
+            $rate = "B";
+        }
+        else if ($final > 69){
+            print ("Your final grade is $final. You got an C.");
+            $rate = "C";
+        }
+        else if ($final > 59){
+            print ("Your final grade is $final. You got an D.");
+            $rate = "D";
+        }
+        else if ($final > 0){
+            print ("Your final grade is $final. You got an F.");
+            $rate = "F";
+        }
+        else {
+            print("Illegal grade less than 0. FInal grade = $final");
+            $rate = "Illegal";
+        }
+        
+        print("<br></br>");
+        
+        switch($rate){
+            case "A": print("Execellent!");break;
+            case "B": print("Good!");break;
+            case "C": print("Not bad!");break;
+            case "D": print("Normal."); break;
+            case "F": print("Try again"); break;
+            default: print("Illegal grade");
+        }
+    ?>
+</body>
+</html>
